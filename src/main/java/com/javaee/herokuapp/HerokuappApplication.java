a739cc4a7a5975177b50fdfcc7340eb20e227c3a
Joao Moreira <jfmoreir@thoughtworks.com>
2018-06-01 09:02:39 -0300

    first commit

---
+++ src/main/java/com/javaee/herokuapp/HerokuappApplication.java
@@ -0,0 +1,12 @@
+package com.javaee.herokuapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuappApplication.class, args);
+	}
+}
